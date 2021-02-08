8f6020c1a3a20030b24db47bd68d17a95d4d51c6
gametagflight <sixb@live.fr>
2021-02-08 11:59:12 +0100

    Add comments

--- netbox_upgrade.sh
+++ netbox_upgrade.sh
@@ -13,9 +13,9 @@ wget https://github.com/netbox-community/netbox/archive/v$NEW.tar.gz
 sudo tar -xzf v$NEW.tar.gz -C /opt
 sudo ln -sfn /opt/netbox-$NEW/ /opt/netbox
 
-sudo cp /opt/netbox-$OLD/local_requirements.txt /opt/netbox/
+sudo cp /opt/netbox-$OLD/local_requirements.txt /opt/netbox/ #May return error if not having local requirements (LDAP, NAPALM...)
 sudo cp /opt/netbox-$OLD/netbox/netbox/configuration.py /opt/netbox/netbox/netbox/
-sudo cp /opt/netbox-$OLD/netbox/netbox/ldap_config.py /opt/netbox/netbox/netbox/
+sudo cp /opt/netbox-$OLD/netbox/netbox/ldap_config.py /opt/netbox/netbox/netbox/ #May return error if not using LDAP
 sudo cp -pr /opt/netbox-$OLD/netbox/media/ /opt/netbox/netbox/
 sudo cp /opt/netbox-$OLD/gunicorn.py /opt/netbox/
 sudo sh /opt/netbox/upgrade.sh
